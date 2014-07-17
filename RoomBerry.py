7df6c859e229acfc194688b2f5d4d7029f4eb155
Harlequeen <github@skwerl.co.uk>
2014-07-17 15:22:02 +0100

    Dots!

--- RoomBerry.py
+++ RoomBerry.py
@@ -134,7 +134,7 @@ class RoomBerry(object):
     self.__screen.blit(self.__text,(10,10))
     # Attempt to get calendar entry
     #self.__appointment=self.__roomFolder.get_event(#NEEED EVENT ID#)
-    self__appointments=self__roomFolder.search_appointments("AgAA",date("17 July 2014"))
+    self__appointments=self.__roomFolder.search_appointments("AgAA",date("17 July 2014"))
     pg.display.flip()
     pg.event.pump()
     pg.display.flip()
