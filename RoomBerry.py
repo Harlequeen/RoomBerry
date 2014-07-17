c3505146dc20f4029bb1b7e7ed77ca1bd59c9273
Harlequeen <github@skwerl.co.uk>
2014-07-17 14:55:50 +0100

    Attempting to find today's appointments
    THis won't work.

--- RoomBerry.py
+++ RoomBerry.py
@@ -134,6 +134,7 @@ class RoomBerry(object):
     self.__screen.blit(self.__text,(10,10))
     # Attempt to get calendar entry
     #self.__appointment=self.__roomFolder.get_event(#NEEED EVENT ID#)
+    self__appointments=search_appointments(self__roomFolder,"AgAA",date("17 July 2014"))
     pg.display.flip()
     pg.event.pump()
     pg.display.flip()
